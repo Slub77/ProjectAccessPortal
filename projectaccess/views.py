e1ce6806b9b648fbe413e3e6ebf7081208904b63
Kalmalyzer <mikael@kalms.org>
2015-08-31 13:06:39 +0200

    Display made-up list of user accounts

--- projectaccess/views.py
+++ projectaccess/views.py
@@ -1,9 +1,27 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
+import random
+
+def generate_random_users(count):
+
+    def generate_random_user():
+        id = random.randint(0, 999999)
+        return {
+            'id': str(id),
+            'name': 'user_name_' + str(id),
+            'email': 'firstname_lastname_' + str(id)
+            }
+
+    users = [generate_random_user() for i in range(count)]
+    
+    return users
+            
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    users = generate_random_users(10)
+    template = loader.get_template('users.html')
+    context = RequestContext(request, {
+        'users': users,
+    })
+    return HttpResponse(template.render(context))

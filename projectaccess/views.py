5f8c77b04c87a230e30f0ac4132cdb875b0c5f81
Kalmalyzer <mikael@kalms.org>
2015-09-03 16:06:05 +0200

    Implemented button for removing a user from a project

--- projectaccess/views.py
+++ projectaccess/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 
 from models import LDAPUser, MetaUser, MetaProject
-from meta_actions import add_user_to_project
+from meta_actions import add_user_to_project, remove_user_from_project
 
 
 def index(request):
@@ -25,12 +25,22 @@ def users(request):
 def projects(request):
 
     if request.method == 'POST':
-        project_id = request.POST['project_id']
-        user_name = request.POST['user_name']
-        ldap_user = LDAPUser.objects.get(uid=user_name)
-        meta_user = MetaUser.objects.get(ldap_user=ldap_user)
-        meta_project = MetaProject.objects.get(id=project_id)
-        add_user_to_project(meta_project, meta_user)
+
+        if request.POST['action'] == 'add':
+            project_id = request.POST['project_id']
+            user_name = request.POST['user_name']
+            ldap_user = LDAPUser.objects.get(uid=user_name)
+            meta_user = MetaUser.objects.get(ldap_user=ldap_user)
+            meta_project = MetaProject.objects.get(id=project_id)
+            add_user_to_project(meta_project, meta_user)
+        elif request.POST['action'] == 'remove':
+            project_id = request.POST['project_id']
+            user_id = request.POST['user_id']
+            meta_user = MetaUser.objects.get(id=user_id)
+            meta_project = MetaProject.objects.get(id=project_id)
+            remove_user_from_project(meta_project, meta_user)
+        else:
+            raise Exception("Unsupported POST action")
 
     template = loader.get_template('projects.html')
     context = RequestContext(request, {

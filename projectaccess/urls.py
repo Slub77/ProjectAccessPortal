e27c13ff079e3f56547d091fbab81dcbd0bd72d1
Kalmalyzer <mikael@kalms.org>
2015-09-14 15:19:58 +0200

    build fix

--- projectaccess/urls.py
+++ projectaccess/urls.py
@@ -11,7 +11,6 @@ urlpatterns = [
     url(r'^my_projects/$', views.my_projects, name='my_projects'),
     url(r'^projects/$', views.projects, name='projects'),
     url(r'^groups/$', views.groups, name='groups'),
-    url(r'^import_p4_users/$', views.import_p4_users, name='import_p4_users'),
     url(r'^create_new_project/$', views.create_new_project, name='create_new_project'),
     url(r'^create_new_project_submit/$', views.create_new_project_submit, name='create_new_project_submit'),
     url(r'^(?P<id>[0-9]+)/delete_project/$', views.delete_project, name='delete_project'),
